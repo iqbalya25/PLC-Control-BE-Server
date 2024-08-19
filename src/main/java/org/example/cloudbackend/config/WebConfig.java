ba9d826d234f0e45e292a28708276eb43364fb08
iqbalya25 <iqbalya25@gmail.com>
2024-08-19 07:17:35 +0700

    implement web config

---
+++ src/main/java/org/example/cloudbackend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.example.cloudbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "plc-control-fe-rev00.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
+
+
