a3997082b848ca2856058e3785b5ed6c579a23f5
iqbalya25 <iqbalya25@gmail.com>
2024-08-19 07:52:22 +0700

    update cors with latest vercel

--- src/main/java/org/example/cloudbackend/config/WebConfig.java
+++ src/main/java/org/example/cloudbackend/config/WebConfig.java
@@ -9,7 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "plc-control-fe-rev00.vercel.app", "https://plc-control-fe-rev00-mq5las86v-iqbalya25s-projects.vercel.app")
+                .allowedOrigins(
+                        "https://plc-control-fe-rev00.vercel.app",
+                        "https://plc-control-fe-rev00-git-main-iqbalya25s-projects.vercel.app",
+                        "https://plc-control-fe-rev00-jgjdptolf-iqbalya25s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
